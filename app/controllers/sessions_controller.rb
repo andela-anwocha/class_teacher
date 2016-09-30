42052c989ea63ec9007b98a864a03bdb02cdeb32
andela-anwocha <adimoranma.felix@andela.com>
2016-09-30 12:57:35 +0100

    feat: implements teacher signin

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    teacher = Teacher.find_by(email: params[:email])
+    if teacher && teacher.authenticate(params[:password])
+      session[:teacher_id] = teacher.id
+      redirect_to "/", notice: "Welcome #{teacher.name}"
+    else
+      flash[:errors] = ["Email or Password Invalid"]
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root "teachers#home"
   resources :teachers
+
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
   get "/signup", to: "teachers#new"
 end
